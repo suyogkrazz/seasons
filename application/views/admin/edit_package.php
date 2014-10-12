3a09b0fa4b1eae38997a85dca52e7ad70d43d1fc
Lalit Thapa <emocentmagar@gmail.com>
2014-10-12 17:02:15 +0545

    delete final

--- application/views/admin/edit_package.php
+++ application/views/admin/edit_package.php
@@ -51,7 +51,6 @@
 	</div>
 
 	<input type="hidden" name="package_id" value="<?php echo $package[0]->id ; ?>" >
-	<input type="hidden" name="id" value="<?php echo $package[0]->id ; ?>" >
 
 	<div class="form-group">
 		<div class="col-sm-2"></div>
